4b083b08825b480815c2f62ba2a5a7b21cad5c25
Gabriel Azevedo <gabriel.azevedo@iv2.com.br>
2019-11-19 17:33:57 -0300

    + min and max functions

--- index.js
+++ index.js
@@ -1,21 +1,52 @@
-var data = [
+var data = [ // Variável do meio é a mais significante no exemplo
     [1, 2, 1],
     [2, 5, 2],
-    [3, 1, 3]    
+    [3, 1, 3]
 ];
-var normalizedData = [];
-var verticalizedData = [];
-var maxes = [];
-var mins = [];
 
-for (var i = 0; i < data.length; i++) {
-    for (var j = 0; j < data[i].length; j++) {
-        if (maxes[j] == undefined) maxes[j] = data[i][j]
-        if (mins[j] == undefined) mins[j] = data[i][j]
-        if (maxes[j] < data[i][j]) maxes[j] = data[i][j];
-        if (mins[j] > data[i][j]) mins[j] = data[i][j];
+var results = [
+    3,
+    7,
+    4
+]
+
+function calculateMaxes(data) {
+    var maxes = [];
+    for (var i = 0; i < data.length; i++) {
+        for (var j = 0; j < data[i].length; j++) {
+            if (maxes[j] == undefined) maxes[j] = data[i][j]
+            if (maxes[j] < data[i][j]) maxes[j] = data[i][j];
+        }
     }
+    return maxes;
 }
 
-console.log(maxes);
-console.log(mins);
+function calculateMins(data) {
+    var mins = [];
+    for (var i = 0; i < data.length; i++) {
+        for (var j = 0; j < data[i].length; j++) {
+            if (mins[j] == undefined) mins[j] = data[i][j]
+            if (mins[j] > data[i][j]) mins[j] = data[i][j];
+        }
+    }
+    return mins;
+}
+
+function normalizeData(data, maxes) {
+    var normalizedData = [];
+    for (var i = 0; i < data.length; i++) {
+        normalizedData[i] = [];
+        for (var j = 0; j < data[i].length; j++) {
+            normalizedData[i][j] = parseFloat((data[i][j] / maxes[j]).toFixed(2));
+        }
+    }
+    return normalizedData;
+}
+
+function varSignificance(normalizedData, results) {
+    
+}
+
+console.log(calculateMins(data));
+console.log(calculateMaxes(data));
+console.log(normalizeData(data, calculateMaxes(data)));
