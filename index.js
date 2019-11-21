a1e727795928453f697b6d32ce1871ff02728324
Gabriel Azevedo <godeazevedo@gmail.com>
2019-11-20 22:28:12 -0300

    Started first stage of most impactful variable definition

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 var data = [ // Variável do meio é a mais significante no exemplo
     [1, 2, 1],
     [2, 5, 2],
-    [3, 1, 3]
+    [3, 1, 3],
 ];
 
 var results = [
-    3,
-    7,
-    4
+    [3],
+    [7],
+    [4]
 ]
 
 function calculateMaxes(data) {
@@ -43,10 +43,33 @@ function normalizeData(data, maxes) {
     return normalizedData;
 }
 
-function varSignificance(normalizedData, results) {
-    
+function calculateMedian(data) {
+    var median = [];
+    for (var i = 0; i < data.length; i++) {
+        for (var j = 0; j < data[i].length; j++) {
+            if (median[j] == undefined) median[j] = 0;
+            median[j] += data[i][j] / data.length;
+        }
+    }
+    return median;
 }
 
-console.log(calculateMins(data));
-console.log(calculateMaxes(data));
-console.log(normalizeData(data, calculateMaxes(data)));
+function calculateDistance(value, median) {
+    return value - median;
+}
+
+function varSignificance(normalizedData, normalizedResults) {
+    console.log(normalizedData);
+    console.log(normalizedResults);
+    console.log(calculateMedian(normalizedData));
+    console.log(calculateMedian(normalizedResults));
+    var dataMed = calculateMedian(normalizedData);
+    var resMed = calculateMedian(normalizedResults);
+    for (var i = 0; i < normalizedData.length; i++) {
+        for (var j = 0; j < normalizedData[i].length; j++) {
+            console.log(calculateDistance(normalizedData[i][j], dataMed[i]));
+        }
+    }
+}
+
+varSignificance(normalizeData(data, calculateMaxes(data)), normalizeData(results, calculateMaxes(results)));
